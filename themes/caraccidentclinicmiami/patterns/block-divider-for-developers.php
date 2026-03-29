c4a49c5c109e8067e6dc057334af00cb74d7e077
Dayrel Almaguer <dayrel.almaguer@gmail.com>
2026-03-29 17:43:23 -0500

    Update block pattern categories for improved organization and clarity

--- themes/caraccidentclinicmiami/patterns/block-divider-for-developers.php
+++ themes/caraccidentclinicmiami/patterns/block-divider-for-developers.php
@@ -3,14 +3,14 @@
 /**
  * Title: Block Divider for Developers
  * Slug: caraccidentclinicmiami/block-divider-for-developers
- * Categories: caraccidentclinicmiami
+ * Categories: caraccidentclinicmiami/dev
  * Description: This pattern has been designed exclusively for developers; its purpose is to make it easier to distinguish between different patterns in the code editor.
  * Keywords: developer, divider, visual aid
  * Post Types: page
  */
 
 ?>
-<!-- wp:group {"metadata":{"categories":["caraccidentclinicmiami"],"patternName":"caraccidentclinicmiami/block-divider-for-developers","name":"Block Divider for Developers"},"align":"full","className":"is-dark developer-divider","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"primary-dark"} -->
+<!-- wp:group {"metadata":{"categories":["caraccidentclinicmiami/dev"],"patternName":"caraccidentclinicmiami/block-divider-for-developers","name":"Block Divider for Developers"},"align":"full","className":"is-dark developer-divider","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"primary-dark"} -->
 <div class="wp-block-group alignfull is-dark developer-divider has-primary-dark-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
 <p class="has-text-align-center has-medium-font-size">🟢🟢🟢🟢</p>
 <!-- /wp:paragraph --></div>
