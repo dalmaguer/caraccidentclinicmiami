5c726b4b8c93d7647feb426e14d1bf490d351e49
Dayrel Almaguer <dayrel.almaguer@gmail.com>
2026-03-29 17:43:36 -0500

    Update top-info-bar pattern layout for improved responsiveness and organization

--- themes/caraccidentclinicmiami/patterns/top-info-bar.php
+++ themes/caraccidentclinicmiami/patterns/top-info-bar.php
@@ -9,13 +9,13 @@
  */
 ?>
 <!-- wp:group {"metadata":{"categories":["caraccidentclinicmiami/content"],"patternName":"caraccidentclinicmiami/top-info-bar","name":"Top Info Bar"},"align":"full","className":"","style":{"spacing":{"padding":{"top":"0.625rem","bottom":"0.625rem","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"backgroundColor":"secondary-light","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-secondary-light-background-color has-background" style="padding-top:0.625rem;padding-right:var(--wp--preset--spacing--50);padding-bottom:0.625rem;padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"className":"flex flex-col justify-evenly md:flex-row md:justify-evenly py-2 gap-4","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull has-secondary-light-background-color has-background" style="padding-top:0.625rem;padding-right:var(--wp--preset--spacing--50);padding-bottom:0.625rem;padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"className":"flex flex-col justify-evenly md:flex-row md:justify-evenly py-2 gap-4","layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group flex flex-col justify-evenly md:flex-row md:justify-evenly py-2 gap-4"><!-- wp:group {"className":"!flex-nowrap ","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group !flex-nowrap"><!-- wp:font-awesome/icon {"iconLayers":[{"iconDefinition":{"iconName":"map-pin","prefix":"fas","icon":[320,512,null,null,"M192 284.4C256.1 269.9 304 212.5 304 144 304 64.5 239.5 0 160 0S16 64.5 16 144c0 68.5 47.9 125.9 112 140.4L128 480c0 17.7 14.3 32 32 32s32-14.3 32-32l0-195.6zM168 96c-30.9 0-56 25.1-56 56 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-57.4 46.6-104 104-104 13.3 0 24 10.7 24 24s-10.7 24-24 24z"]},"spin":false,"transform":null}]} -->
 <div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-pin" class="svg-inline--fa fa-map-pin " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M192 284.4C256.1 269.9 304 212.5 304 144 304 64.5 239.5 0 160 0S16 64.5 16 144c0 68.5 47.9 125.9 112 140.4L128 480c0 17.7 14.3 32 32 32s32-14.3 32-32l0-195.6zM168 96c-30.9 0-56 25.1-56 56 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-57.4 46.6-104 104-104 13.3 0 24 10.7 24 24s-10.7 24-24 24z"></path></svg></div>
 <!-- /wp:font-awesome/icon -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.875rem","lineHeight":"1"}},"textColor":"primary-dark"} -->
+<!-- wp:paragraph {"className":" ","style":{"typography":{"fontSize":"0.875rem","lineHeight":"1"}},"textColor":"primary-dark"} -->
 <p class="has-primary-dark-color has-text-color" style="font-size:0.875rem;line-height:1">123 Example Street, Miami, Florida, 33101</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
@@ -25,7 +25,7 @@
 <div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="clock" class="svg-inline--fa fa-clock " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 256a208 208 0 1 1 -416 0 208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0 256 256 0 1 0 -512 0zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"></path></svg></div>
 <!-- /wp:font-awesome/icon -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.875rem","lineHeight":"1"}},"textColor":"primary-dark"} -->
+<!-- wp:paragraph {"className":"","style":{"typography":{"fontSize":"0.875rem","lineHeight":"1"}},"textColor":"primary-dark"} -->
 <p class="has-primary-dark-color has-text-color" style="font-size:0.875rem;line-height:1">Mon - Fri 08.00 - 18.00 / Sat - Sun 09.00 - 14.00</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
@@ -35,7 +35,7 @@
 <div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" class="svg-inline--fa fa-phone " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M160.2 25C152.3 6.1 131.7-3.9 112.1 1.4l-5.5 1.5c-64.6 17.6-119.8 80.2-103.7 156.4 37.1 175 174.8 312.7 349.8 349.8 76.3 16.2 138.8-39.1 156.4-103.7l1.5-5.5c5.4-19.7-4.7-40.3-23.5-48.1l-97.3-40.5c-16.5-6.9-35.6-2.1-47 11.8l-38.6 47.2C233.9 335.4 177.3 277 144.8 205.3L189 169.3c13.9-11.3 18.6-30.4 11.8-47L160.2 25z"></path></svg></div>
 <!-- /wp:font-awesome/icon -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.875rem","lineHeight":"1"}},"textColor":"primary-dark"} -->
+<!-- wp:paragraph {"className":"","style":{"typography":{"fontSize":"0.875rem","lineHeight":"1"}},"textColor":"primary-dark"} -->
 <p class="has-primary-dark-color has-text-color" style="font-size:0.875rem;line-height:1">(305) 555-0199</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
