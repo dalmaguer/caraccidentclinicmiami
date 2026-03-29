69c2b65c088a8b07fc29a49c09b0c8db561f8797
Dayrel Almaguer <dayrel.almaguer@gmail.com>
2026-03-29 12:02:37 -0500

    Add Block Divider pattern for developers to enhance code organization

---
+++ themes/caraccidentclinicmiami/patterns/block-divider-for-developers.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Title: Block Divider for Developers
+ * Slug: caraccidentclinicmiami/block-divider-for-developers
+ * Categories: caraccidentclinicmiami
+ * Description: This pattern has been designed exclusively for developers; its purpose is to make it easier to distinguish between different patterns in the code editor.
+ * Keywords: developer, divider, visual aid
+ * Post Types: page
+ */
+
+?>
+<!-- wp:group {"align":"full","backgroundColor":"primary-dark","isDark":true,"className":"developer-divider alignfull","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-group alignfull is-dark developer-divider has-primary-dark-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+<!-- wp:paragraph {"align":"center","textColor":"primary-dark","fontSize":"medium"} -->
+<p class="has-text-align-center has-primary-dark-color has-text-color has-medium-font-size">🟢🟢🟢</p>
+<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
