68ed6c84a798c05cf778a0fc7236210779d97522
Dayrel Almaguer <dayrel.almaguer@gmail.com>
2026-03-30 20:45:54 -0500

    Update doctors expert specialty pattern to refine category structure and adjust column widths

--- themes/caraccidentclinicmiami/patterns/doctors-expert-specialty.php
+++ themes/caraccidentclinicmiami/patterns/doctors-expert-specialty.php
@@ -10,12 +10,11 @@
 $img_square    = get_template_directory_uri() . '/assets/images/placeholder_square.png';
 $img_16x9 = get_template_directory_uri() . '/assets/images/placeholder_16x9.png';
 ?>
-
-<!-- wp:group {"metadata":{"patternName":"caraccidentclinicmiami/doctors-expert-specialty","name":"Doctors - Expert in Specialty","categories":["caraccidentclinicmiami"]},"align":"full","style":{"spacing":{"padding":{"top":"5rem","bottom":"5rem","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"backgroundColor":"light-gray"} -->
+<!-- wp:group {"metadata":{"patternName":"caraccidentclinicmiami/doctors-expert-specialty","name":"Doctors - Expert in Specialty","categories":["caraccidentclinicmiami","caraccidentclinicmiami/content"]},"align":"full","style":{"spacing":{"padding":{"top":"5rem","bottom":"5rem","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"backgroundColor":"light-gray"} -->
 <div class="wp-block-group alignfull has-light-gray-background-color has-background" style="padding-top:5rem;padding-right:var(--wp--preset--spacing--50);padding-bottom:5rem;padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"constrained","contentSize":"1200px","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"3rem","left":"var:preset|spacing|80"}}}} -->
-<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"40%","style":{"spacing":{"blockGap":"1.5rem"}}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:40%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.75rem","letterSpacing":"0.2em","textTransform":"uppercase","fontWeight":"700"}},"textColor":"primary-dark"} -->
+<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"blockGap":{"top":"3rem","left":"var:preset|spacing|80"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"50%","style":{"spacing":{"blockGap":"1.5rem"}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.75rem","letterSpacing":"0.2em","textTransform":"uppercase","fontWeight":"700"}},"textColor":"primary-dark"} -->
 <p class="has-primary-dark-color has-text-color" style="font-size:0.75rem;font-weight:700;letter-spacing:0.2em;text-transform:uppercase">Doctors</p>
 <!-- /wp:paragraph -->
 
@@ -48,8 +47,8 @@ $img_16x9 = get_template_directory_uri() . '/assets/images/placeholder_16x9.png'
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center","width":"60%","className":"flex flex-col justify-start "} -->
-<div class="wp-block-column is-vertically-aligned-center flex flex-col justify-start" style="flex-basis:60%"><!-- wp:group {"className":"cac-doctors-images-flex items-start is-layout-flex wp-block-group-is-layout-flex","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<!-- wp:column {"verticalAlignment":"center","width":"50%","className":"flex flex-col justify-start "} -->
+<div class="wp-block-column is-vertically-aligned-center flex flex-col justify-start" style="flex-basis:50%"><!-- wp:group {"className":"cac-doctors-images-flex items-start is-layout-flex wp-block-group-is-layout-flex","layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group cac-doctors-images-flex items-start is-layout-flex wp-block-group-is-layout-flex"><!-- wp:group {"className":"w-5/12 md:w-4/12 overflow-hidden z-10 !mt-10 md:!mt-20 shadow-xl","layout":{"type":"default"}} -->
 <div class="wp-block-group w-5/12 md:w-4/12 overflow-hidden z-10 !mt-10 md:!mt-20 shadow-xl"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"w-full"} -->
 <figure class="wp-block-image size-full w-full"><img src="<?php echo esc_url($img_square) ?>" alt="Doctor consultation"/></figure>
